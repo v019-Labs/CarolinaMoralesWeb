99c9097f465f5abab98bcfb906c22a937c74b2ba
v019-exe <v019.exe@gmail.com>
2026-01-01 20:46:55 +0000

    improved responsive

--- components/service-page-client.tsx
+++ components/service-page-client.tsx
@@ -78,11 +78,11 @@ export function ServicePageClient({ slug }: ServicePageClientProps) {
 
                     {slug === 'nacionalidad' && (
                         <div className="mb-16 flex items-center justify-center">
-                            <div className="bg-background relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border px-40 pt-8 pb-40 md:pb-60">
-                                <span className="pointer-events-none bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-6xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-white dark:to-slate-900/10">
+                            <div className="bg-background relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border px-4 md:px-40 pt-8 pb-40 md:pb-60">
+                                <span className="pointer-events-none bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-4xl md:text-6xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-white dark:to-slate-900/10 z-10">
                                     Nacionalidad
                                 </span>
-                                <Globe className="top-28" />
+                                <Globe className="top-20 md:top-28" />
                                 <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
                             </div>
                         </div>
